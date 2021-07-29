ceae49317ff87b6e79f60b090d9a4e435da09934
Rahul Masule <85429689+RahulMasule@users.noreply.github.com>
2021-07-29 15:39:15 +0530

    Delete SBProj3-FileUploadAndDownload directory

--- SBProj3-FileUploadAndDownload/src/main/java/com/rahul/service/IFileUploadDwonloadService.java
+++
@@ -1,8 +0,0 @@
-package com.rahul.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface IFileUploadDwonloadService {
-
-	public String uplodFileToLocalSystem(MultipartFile file);
-}
